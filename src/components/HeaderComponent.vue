2450e59efcb755682efe4bb7b88ea246aa322231
Felipe Peterle <felipepeterle9@gmail.com>
2025-05-07 15:25:45 -0300

    Feat:alterando arquivos iniciais

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,44 @@
+<!-- src/components/HeaderComponent.vue -->
+<template>
+    <header class="header">
+      <h1>{{ titulo }}</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderComponent',
+    data() {
+      return {
+        titulo: 'Felipe Peterle'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .header {
+    background-color: #222;
+    color: #fff;
+    padding: 1rem;
+  }
+  nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+    margin: 0;
+  }
+  nav a {
+    color: white;
+    text-decoration: none;
+  }
+  </style>
+  
